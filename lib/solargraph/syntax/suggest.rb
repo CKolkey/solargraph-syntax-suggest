17942879e015f21fe4e38ce739cda7860a5bbf37
Cameron <Alleyria@gmail.com>
2022-12-24 12:11:03 +0100

    initial commit

---
+++ lib/solargraph/syntax/suggest.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "suggest/version"
+
+module Solargraph
+  module Syntax
+    module Suggest
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/solargraph/syntax/suggest/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Solargraph
+  module Syntax
+    module Suggest
+      VERSION = "0.1.0"
+    end
+  end
+end
